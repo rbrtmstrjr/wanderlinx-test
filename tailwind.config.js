1cbdcfe9421d4a8d6c3ac45ab4323d7a84ef48dd
rbrtmstrjr <robertmaestro09@gmail.com>
2023-09-21 21:02:12 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#353f4f",
+        secondary: "#47b8c1",
+        tertiary: "#353f4f",
+        light: "#e6e2f9",
+        lighter: "#f6f5fa",
+        accent: "#5a56c7",
+      },
+      fontFamily: {
+        primary: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "pricing-bg":
+          "url('https://cdn.mywanderlinx.com/site/images/bg-05.jpg')",
+      },
+    },
   },
   plugins: [],
 };
